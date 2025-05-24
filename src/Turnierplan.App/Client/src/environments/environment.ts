1ea7b24f89719eed93240fc0770634178c4fba82
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-05-24 11:32:32 +0200

    Changes

--- src/Turnierplan.App/Client/src/environments/environment.ts
+++ src/Turnierplan.App/Client/src/environments/environment.ts
@@ -1,19 +1,6 @@
-const compileVersionNumber = (): string => {
-  const now = new Date();
-
-  const start = new Date(now.getFullYear(), 0, 0);
-  const diff = now.getTime() - start.getTime();
-  const oneDay = 1000 * 60 * 60 * 24;
-  const dayOfYear = Math.floor(diff / oneDay);
-  const secondsOfDay = Math.floor((now.getTime() % (1000 * 60 * 60 * 24)) / 1000);
-
-  const nowString = `${now.getFullYear()}${dayOfYear}.${secondsOfDay}`;
-  return `dev-${nowString}`;
-};
-
 export const environment = {
   production: false,
   defaultTitle: 'turnierplan.NET',
-  version: compileVersionNumber(),
+  version: '0.0.0',
   originOverwrite: 'http://localhost:45000'
 };
