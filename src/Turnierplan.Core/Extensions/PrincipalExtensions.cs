b281edcc12eaced52de46347b2eeb5bd116b2241
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-06-21 07:54:58 +0200

    Add important TODO

--- src/Turnierplan.Core/Extensions/PrincipalExtensions.cs
+++ src/Turnierplan.Core/Extensions/PrincipalExtensions.cs
@@ -2,6 +2,8 @@ using Turnierplan.Core.RoleAssignment;
 
 namespace Turnierplan.Core.Extensions;
 
+// TODO: Add a new field "PrincipalId" for ApiKey & User | OR Make the Id of APikey also a Guid
+//       This is especially important for ApiKeys, so that the incrementally generated id is not "leaked"
 public static class PrincipalExtensions
 {
     public static Principal AsPrincipal(this ApiKey.ApiKey apiKey)
