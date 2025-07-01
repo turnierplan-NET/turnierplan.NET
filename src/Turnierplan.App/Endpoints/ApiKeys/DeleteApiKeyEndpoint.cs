9efebdf43dbdcf677788e3d618dbe7e89ac5b815
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-07-01 20:39:51 +0200

    Fix wrong permission check for api keys

--- src/Turnierplan.App/Endpoints/ApiKeys/DeleteApiKeyEndpoint.cs
+++ src/Turnierplan.App/Endpoints/ApiKeys/DeleteApiKeyEndpoint.cs
@@ -26,7 +26,7 @@ internal sealed class DeleteApiKeyEndpoint : EndpointBase
             return Results.NotFound();
         }
 
-        if (!accessValidator.IsActionAllowed(apiKey.Organization, Actions.GenericDelete))
+        if (!accessValidator.IsActionAllowed(apiKey, Actions.GenericDelete))
         {
             return Results.Forbid();
         }
