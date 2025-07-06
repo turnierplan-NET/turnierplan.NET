eed8c0454476abc66581513bed9f33669c39daab
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-07-06 19:20:14 +0200

    test

--- src/Turnierplan.Core.Test.Unit/Tournament/TeamGroupResultsTest.cs
+++ src/Turnierplan.Core.Test.Unit/Tournament/TeamGroupResultsTest.cs
@@ -56,6 +56,58 @@ public sealed class TeamGroupStatisticsTest
         }
     };
 
+    public static readonly TheoryData<(int GoalsFor, int GoalsAgainst)[], TeamGroupStatistics> AddMatchOutcomeWhenHigherScoreLosesTestData = new()
+    {
+        {
+            [
+                (GoalsFor: 2, GoalsAgainst: 3),
+                (GoalsFor: 1, GoalsAgainst: 1),
+                (GoalsFor: 4, GoalsAgainst: 0)
+            ],
+            new TeamGroupStatistics
+            {
+                ScoreFor = 2 + 1 + 4,
+                ScoreAgainst = 3 + 1 + 0,
+                MatchesWon = 1,
+                MatchesDrawn = 1,
+                MatchesLost = 1,
+                Points = 4
+            }
+        },
+        {
+            [
+                (GoalsFor: 1, GoalsAgainst: 0),
+                (GoalsFor: 2, GoalsAgainst: 2)
+            ],
+            new TeamGroupStatistics
+            {
+                ScoreFor = 1 + 2,
+                ScoreAgainst = 0 + 2,
+                MatchesWon = 0,
+                MatchesDrawn = 1,
+                MatchesLost = 1,
+                Points = 1
+            }
+        },
+        {
+            [
+                (GoalsFor: 1, GoalsAgainst: 1),
+                (GoalsFor: 2, GoalsAgainst: 0),
+                (GoalsFor: 1, GoalsAgainst: 3),
+                (GoalsFor: 2, GoalsAgainst: 2)
+            ],
+            new TeamGroupStatistics
+            {
+                ScoreFor = 1 + 2 + 1 + 2,
+                ScoreAgainst = 1 + 0 + 3 + 2,
+                MatchesWon = 1,
+                MatchesDrawn = 2,
+                MatchesLost = 1,
+                Points = 5
+            }
+        }
+    };
+
     [Theory]
     [InlineData(7, 3, 4)]
     [InlineData(3, 7, -4)]
@@ -101,7 +153,7 @@ public sealed class TeamGroupStatisticsTest
 
     [Theory]
     [MemberData(nameof(AddMatchOutcomeTestData))]
-    public void TeamGroupResults___Add_Match_Outcome_With_Custom_Computation_Configuration___Works_As_Expected((int GoalsFor, int GoalsAgainst)[] matchOutcomes, TeamGroupStatistics expectedResults)
+    public void TeamGroupResults___Add_Match_Outcome_With_Custom_Point_Counts___Works_As_Expected((int GoalsFor, int GoalsAgainst)[] matchOutcomes, TeamGroupStatistics expectedResults)
     {
         // Arrange
         var results = new TeamGroupStatistics();
@@ -118,5 +170,23 @@ public sealed class TeamGroupStatisticsTest
         {
             Points = expectedResults.MatchesDrawn * 2 + expectedResults.MatchesWon * 5
         });
+    }
+
+    [Theory]
+    [MemberData(nameof(AddMatchOutcomeWhenHigherScoreLosesTestData))]
+    public void TeamGroupResults___Add_Match_Outcome_With_HigherScoreLoses_Enabled___Works_As_Expected((int GoalsFor, int GoalsAgainst)[] matchOutcomes, TeamGroupStatistics expectedResults)
+    {
+        // Arrange
+        var results = new TeamGroupStatistics();
+        var configuration = new ComputationConfiguration { HigherScoreLoses = true };
+
+        // Act
+        foreach (var matchOutcome in matchOutcomes)
+        {
+            results.AddMatchOutcome(matchOutcome.GoalsFor, matchOutcome.GoalsAgainst, configuration);
+        }
+
+        // Assert
+        results.Should().BeEquivalentTo(expectedResults);
     }
 }
