4976e546ed19728438834d18398bc8c1c0901328
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-07-06 10:01:35 +0200

    Add further customizability options

--- src/Turnierplan.App/Options/TurnierplanOptions.cs
+++ src/Turnierplan.App/Options/TurnierplanOptions.cs
@@ -4,6 +4,10 @@ internal sealed record TurnierplanOptions
 {
     public string? ApplicationUrl { get; init; }
 
+    public string? InstanceName { get; init; }
+
+    public string? LogoUrl { get; init; }
+
     public string? ImprintUrl { get; init; }
 
     public string? PrivacyUrl { get; init; }
