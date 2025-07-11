fb76d96a99ce1b93865ffbff7e4309f36f40931f
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-07-11 13:36:55 +0200

    Remove 'IsMigrated' property from tournament entity (#85)

---
+++ src/Turnierplan.Dal/Migrations/20250711112755_Remove_IsMigrated.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Turnierplan.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_IsMigrated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsMigrated",
+                schema: "turnierplan",
+                table: "Tournaments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMigrated",
+                schema: "turnierplan",
+                table: "Tournaments",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
