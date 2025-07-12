db71e57d0a186b2c569f6ac3b7f8178f0e6b2d0f
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-07-12 08:20:04 +0200

    add EF migration

---
+++ src/Turnierplan.Dal/Migrations/20250712061920_Remove_LengthLimitations.cs
@@ -0,0 +1,400 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Turnierplan.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_LengthLimitations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "Venues",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "turnierplan",
+                table: "Venues",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedEMail",
+                schema: "turnierplan",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EMail",
+                schema: "turnierplan",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "Tournaments",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "Teams",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "Organizations",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(40)",
+                oldMaxLength: 40);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TeamSelectorB",
+                schema: "turnierplan",
+                table: "Matches",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TeamSelectorA",
+                schema: "turnierplan",
+                table: "Matches",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "Images",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileType",
+                schema: "turnierplan",
+                table: "Images",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(5)",
+                oldMaxLength: 5);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplayName",
+                schema: "turnierplan",
+                table: "Groups",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(25)",
+                oldMaxLength: 25,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "Folders",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "Documents",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Configuration",
+                schema: "turnierplan",
+                table: "Documents",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1024)",
+                oldMaxLength: 1024);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "ApiKeys",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "turnierplan",
+                table: "ApiKeys",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(250)",
+                oldMaxLength: 250);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Path",
+                schema: "turnierplan",
+                table: "ApiKeyRequests",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(150)",
+                oldMaxLength: 150);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "Venues",
+                type: "character varying(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "turnierplan",
+                table: "Venues",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedEMail",
+                schema: "turnierplan",
+                table: "Users",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "Users",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EMail",
+                schema: "turnierplan",
+                table: "Users",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "Tournaments",
+                type: "character varying(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "Teams",
+                type: "character varying(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "Organizations",
+                type: "character varying(40)",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TeamSelectorB",
+                schema: "turnierplan",
+                table: "Matches",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TeamSelectorA",
+                schema: "turnierplan",
+                table: "Matches",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "Images",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileType",
+                schema: "turnierplan",
+                table: "Images",
+                type: "character varying(5)",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplayName",
+                schema: "turnierplan",
+                table: "Groups",
+                type: "character varying(25)",
+                maxLength: 25,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "Folders",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "Documents",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Configuration",
+                schema: "turnierplan",
+                table: "Documents",
+                type: "character varying(1024)",
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "turnierplan",
+                table: "ApiKeys",
+                type: "character varying(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "turnierplan",
+                table: "ApiKeys",
+                type: "character varying(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Path",
+                schema: "turnierplan",
+                table: "ApiKeyRequests",
+                type: "character varying(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
