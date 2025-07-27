b4eae6b31df3abc02f5fb71f280e32c289c218e1
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-07-27 11:24:34 +0200

    EnumerableExtensionsTest

--- src/Turnierplan.App/Extensions/EnumerableExtensions.cs
+++ src/Turnierplan.App/Extensions/EnumerableExtensions.cs
@@ -2,7 +2,7 @@ namespace Turnierplan.App.Extensions;
 
 internal static class EnumerableExtensions
 {
-    public static bool None<T>(this IEnumerable<T> enumerable, Func<T, bool> predicate) // TODO: Add unit test
+    public static bool None<T>(this IEnumerable<T> enumerable, Func<T, bool> predicate)
     {
         return !enumerable.Any(predicate);
     }
