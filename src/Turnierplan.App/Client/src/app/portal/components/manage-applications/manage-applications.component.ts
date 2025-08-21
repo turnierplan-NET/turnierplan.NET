14a39b32c6dc9a96002ae9f47fa8e264626caef0
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-08-21 12:02:02 +0200

    Load applications from backend

--- src/Turnierplan.App/Client/src/app/portal/components/manage-applications/manage-applications.component.ts
+++ src/Turnierplan.App/Client/src/app/portal/components/manage-applications/manage-applications.component.ts
@@ -1,18 +1,46 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
 import { ApplicationsFilter } from '../../models/applications-filter';
-import { ReplaySubject } from 'rxjs';
+import { ReplaySubject, switchMap } from 'rxjs';
+import { ApplicationsService, PlanningRealmDto } from '../../../api';
+import { PaginationResultDtoOfApplicationDto } from '../../../api/models/pagination-result-dto-of-application-dto';
 
 @Component({
   standalone: false,
   selector: 'tp-manage-applications',
   templateUrl: './manage-applications.component.html'
 })
-export class ManageApplicationsComponent {
-  // TODO private?
-  protected readonly filter$ = new ReplaySubject<ApplicationsFilter>();
+export class ManageApplicationsComponent implements OnDestroy {
+  @Input()
+  public planningRealm!: PlanningRealmDto;
+
+  @Output()
+  public errorOccured = new EventEmitter<unknown>();
+
+  protected result?: PaginationResultDtoOfApplicationDto;
+
+  private readonly filter$ = new ReplaySubject<ApplicationsFilter>();
+
+  constructor(private readonly applicationsService: ApplicationsService) {
+    this.filter$
+      .pipe(
+        switchMap((filter) => this.applicationsService.getApplications({ planningRealmId: this.planningRealm.id, page: 0, pageSize: 20 }))
+      )
+      .subscribe({
+        next: (result) => {
+          this.result = result;
+        },
+        error: (error) => {
+          this.errorOccured.emit(error);
+        }
+      });
+  }
 
   @Input()
   public set filter(value: ApplicationsFilter) {
     this.filter$.next(value);
+  }
+
+  public ngOnDestroy(): void {
+    this.filter$.complete();
   }
 }
