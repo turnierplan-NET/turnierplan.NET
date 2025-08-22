f339c7b4ff37b29ff172c7c54324febb579107db
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-08-22 12:26:44 +0200

    Always show 'unsaved changes' even when scrolling down (#109)

--- src/Turnierplan.App/Client/src/app/portal/components/unsaved-changes-alert/unsaved-changes-alert.component.ts
+++ src/Turnierplan.App/Client/src/app/portal/components/unsaved-changes-alert/unsaved-changes-alert.component.ts
@@ -1,14 +1,49 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   standalone: false,
   selector: 'tp-unsaved-changes-alert',
   templateUrl: './unsaved-changes-alert.component.html'
 })
-export class UnsavedChangesAlertComponent {
+export class UnsavedChangesAlertComponent implements OnInit, OnDestroy {
   @Input()
   public inProgress: boolean = false;
 
   @Output()
   public save = new EventEmitter<void>();
+
+  @ViewChild('wrapperDiv')
+  protected wrapperDiv!: ElementRef<HTMLDivElement>;
+
+  private readonly eventListener = () => this.update();
+  private currentTranslateY = 0;
+
+  public ngOnInit(): void {
+    window.addEventListener('scroll', this.eventListener);
+    setTimeout(() => this.update(), 0);
+  }
+
+  public ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.eventListener);
+  }
+
+  protected update(): void {
+    const y = this.wrapperDiv.nativeElement.getBoundingClientRect().top - this.currentTranslateY;
+    let translateY = 0;
+
+    if (y < 70) {
+      translateY = 70 - y;
+    }
+
+    this.wrapperDiv.nativeElement.style.transform = `translateY(${translateY}px)`;
+
+    let shadow = Math.floor(Math.min(translateY / 3, 15));
+    this.wrapperDiv.nativeElement.style.boxShadow = `0 0 ${shadow}px 5px #fff`;
+
+    let margin = Math.floor(Math.min(translateY / 4, 10));
+    this.wrapperDiv.nativeElement.style.marginLeft = `${margin}px`;
+    this.wrapperDiv.nativeElement.style.marginRight = `${margin}px`;
+
+    this.currentTranslateY = translateY;
+  }
 }
