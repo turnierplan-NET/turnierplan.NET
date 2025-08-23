496390759556521ea9268dd6d3a59930482273ba
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-08-23 11:01:54 +0200

    Filter for tag

--- src/Turnierplan.App/Endpoints/Applications/GetApplicationsEndpoint.cs
+++ src/Turnierplan.App/Endpoints/Applications/GetApplicationsEndpoint.cs
@@ -65,14 +65,27 @@ internal sealed class GetApplicationsEndpoint : EndpointBase<PaginationResultDto
 
             if (!string.IsNullOrWhiteSpace(_searchTerm))
             {
+                // If the search term is exactly 6 digits long, we expect that the user wants to search for a specific
+                // tag. However, the user might also want to search for a specific telephone number or email address via
+                // these 6 digits. This is considered in the search logic below.
+
+                int? searchTag = _searchTerm.Length == 6 && _searchTerm.All(char.IsNumber)
+                    ? int.Parse(_searchTerm)
+                    : null;
+
                 applications = applications.Where(x =>
                 {
+                    if (searchTag.HasValue && x.Tag == searchTag)
+                    {
+                        return true;
+                    }
+
                     return x.Notes.Contains(_searchTerm, StringComparison.OrdinalIgnoreCase)
-                        || x.Contact.Contains(_searchTerm, StringComparison.OrdinalIgnoreCase)
-                        || x.ContactEmail?.Contains(_searchTerm, StringComparison.OrdinalIgnoreCase) == true
-                        || x.ContactTelephone?.Contains(_searchTerm, StringComparison.OrdinalIgnoreCase) == true
-                        || x.Comment?.Contains(_searchTerm, StringComparison.OrdinalIgnoreCase) == true
-                        || x.Teams.Any(team => team.Name.Contains(_searchTerm, StringComparison.OrdinalIgnoreCase));
+                           || x.Contact.Contains(_searchTerm, StringComparison.OrdinalIgnoreCase)
+                           || x.ContactEmail?.Contains(_searchTerm, StringComparison.OrdinalIgnoreCase) == true
+                           || x.ContactTelephone?.Contains(_searchTerm, StringComparison.OrdinalIgnoreCase) == true
+                           || x.Comment?.Contains(_searchTerm, StringComparison.OrdinalIgnoreCase) == true
+                           || x.Teams.Any(team => team.Name.Contains(_searchTerm, StringComparison.OrdinalIgnoreCase));
                 });
             }
 
