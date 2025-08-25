62b7568e1886fb1fc9b896e13c82208872b86f89
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-08-25 13:20:22 +0200

    Rename 'TurnierplanMetadata' class to 'TurnierplanVersion' (#118)

--- src/Turnierplan.App/Constants/TurnierplanVersion.cs
+++ src/Turnierplan.App/Constants/TurnierplanVersion.cs
@@ -1,12 +1,12 @@
 namespace Turnierplan.App.Constants;
 
-internal static class TurnierplanMetadata
+internal static class TurnierplanVersion
 {
     public static readonly string Version = DetermineVersion();
 
     private static string DetermineVersion()
     {
-        var assemblyVersion = typeof(TurnierplanMetadata).Assembly.GetName().Version?.ToString();
+        var assemblyVersion = typeof(TurnierplanVersion).Assembly.GetName().Version?.ToString();
 
         return assemblyVersion?[..assemblyVersion.LastIndexOf('.')] ?? "?.?.?";
     }
