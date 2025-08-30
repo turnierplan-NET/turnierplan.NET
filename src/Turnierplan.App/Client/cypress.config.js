0d477643e45af6b7d7903f59b5cfdeeb5ff64772
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-08-30 21:36:18 +0200

    default viewport size

--- src/Turnierplan.App/Client/cypress.config.js
+++ src/Turnierplan.App/Client/cypress.config.js
@@ -5,6 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       config.baseUrl = 'http://localhost:45001';
 
+      config.viewportWidth = 1600;
+      config.viewportHeight = 900;
+
       return config;
     }
   }
