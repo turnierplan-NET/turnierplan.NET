6978374e056484708e0c0267b1daa8241b68c0ac
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-09-14 12:15:21 +0200

    Delete unused repo

--- src/Turnierplan.Core/PlanningRealm/ITeamLinkRepository.cs
+++
@@ -1,8 +0,0 @@
-using Turnierplan.Core.SeedWork;
-
-namespace Turnierplan.Core.PlanningRealm;
-
-public interface ITeamLinkRepository : IRepository<TeamLink, long>
-{
-    // TODO: Add required methods - OR - remove repository if not required
-}
--- src/Turnierplan.Dal/Repositories/TeamLinkRepository.cs
+++
@@ -1,5 +0,0 @@
-using Turnierplan.Core.PlanningRealm;
-
-namespace Turnierplan.Dal.Repositories;
-
-internal sealed class TeamLinkRepository(TurnierplanContext context) : RepositoryBase<TeamLink, long>(context, context.TeamLinks), ITeamLinkRepository;
