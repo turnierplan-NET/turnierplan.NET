b22fa044c7be9336157a6fdd8b69538b4979c336
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-10-13 19:46:07 +0200

    rm

--- src/Turnierplan.App/Endpoints/Applications/DeleteApplicationEndpoint.cs
+++
@@ -1,6 +0,0 @@
-namespace Turnierplan.App.Endpoints.Applications;
-
-internal sealed class DeleteApplicationEndpoint
-{
-    // TODO: Think about conditions for deletion and implement the endpoint + UI
-}
