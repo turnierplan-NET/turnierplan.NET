a73f011d0703b2ed076dd6ee8ef32f943278cbf8
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-11-01 15:48:30 +0100

    Implement storage and event emitter

--- src/Turnierplan.App/Client/src/app/portal/components/auto-reload-toggle/auto-reload-toggle.component.ts
+++ src/Turnierplan.App/Client/src/app/portal/components/auto-reload-toggle/auto-reload-toggle.component.ts
@@ -1,4 +1,8 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
+import { LocalStorageService } from '../../services/local-storage.service';
+import { AutoReloadConfig } from '../../models/auto-reload-config';
+
+const defaultConfig: AutoReloadConfig = { enableAutoReload: true, autoReloadInterval: 60 };
 
 @Component({
   selector: 'tp-auto-reload-toggle',
@@ -9,8 +13,23 @@ export class AutoReloadToggleComponent implements OnInit {
   @Output()
   public pathSuffixChanged = new EventEmitter<string>();
 
+  protected config: AutoReloadConfig;
+
+  private readonly localStorageService = inject(LocalStorageService);
+
+  constructor() {
+    this.config = this.localStorageService.getShareWidgetAutoReloadInterval() ?? defaultConfig;
+  }
+
   public ngOnInit(): void {
-    // TODO bla
-    this.pathSuffixChanged.emit('&autoReload=5');
+    this.emitCurrentValue();
+  }
+
+  private emitCurrentValue(): void {
+    if (this.config.enableAutoReload && this.config.autoReloadInterval > 0) {
+      this.pathSuffixChanged.emit(`&autoReload=${this.config.autoReloadInterval}`);
+    } else {
+      this.pathSuffixChanged.emit('');
+    }
   }
 }
