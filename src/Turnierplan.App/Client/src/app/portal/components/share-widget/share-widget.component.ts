a065a1dbae11d085b86e352f1af6715e7324d5d7
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-11-30 15:07:50 +0100

    UI to enable/disable

--- src/Turnierplan.App/Client/src/app/portal/components/share-widget/share-widget.component.ts
+++ src/Turnierplan.App/Client/src/app/portal/components/share-widget/share-widget.component.ts
@@ -1,22 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { SafeUrl } from '@angular/platform-browser';
-import { TranslateDirective } from '@ngx-translate/core';
+import { TranslateDirective, TranslateService } from '@ngx-translate/core';
 import { NgClass } from '@angular/common';
 import { ShareLinkComponent } from '../share-link/share-link.component';
 import { QRCodeComponent } from 'angularx-qrcode';
 import { AutoReloadToggleComponent } from '../auto-reload-toggle/auto-reload-toggle.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
   selector: 'tp-share-widget',
   templateUrl: './share-widget.component.html',
-  imports: [TranslateDirective, NgClass, ShareLinkComponent, QRCodeComponent, AutoReloadToggleComponent]
+  imports: [TranslateDirective, NgClass, ShareLinkComponent, QRCodeComponent, AutoReloadToggleComponent, ReactiveFormsModule, FormsModule]
 })
 export class ShareWidgetComponent {
   @Input()
   public translationKey: string = '';
-
-  @Input()
-  public showQrCode: boolean = true;
 
   @Input()
   public viewsCounter?: number;
@@ -25,7 +24,7 @@ export class ShareWidgetComponent {
   public resourcePath: string = '';
 
   @Input()
-  public supportsAutoReload = false;
+  public isForFullscreenView = false;
 
   @Input()
   public autoReloadMinInterval: number = 3;
@@ -33,10 +32,19 @@ export class ShareWidgetComponent {
   protected downloadName?: string;
   protected qrCodeUrl?: SafeUrl;
   protected autoReloadPathSuffix = '';
+  protected includeQrCodeOnFullscreenView: boolean;
+
+  constructor(private readonly localStorageService: LocalStorageService) {
+    this.includeQrCodeOnFullscreenView = localStorageService.getIncludeQrCodeOnFullscreenView();
+  }
 
   @Input()
   public set resourceName(value: string) {
     const sanitizer = /[^A-Za-z0-9 _.+-]/;
     this.downloadName = `${value.replace(sanitizer, '_')}.png`;
+  }
+
+  protected get showQrCode(): boolean {
+    return !this.isForFullscreenView;
   }
 }
