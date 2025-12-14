098e98e69b600726af7eeacbe2e2df9270b10ade
Elias Hörner <elias.hoerner@hoerner-it.net>
2025-12-14 19:28:32 +0100

    Small refactoring inside RAnking class

--- src/Turnierplan.App/Mapping/Rules/TournamentMappingRule.cs
+++ src/Turnierplan.App/Mapping/Rules/TournamentMappingRule.cs
@@ -118,7 +118,7 @@ internal sealed class TournamentMappingRule : MappingRuleBase<Tournament, Tourna
                     OutcomeType = match.OutcomeType
                 };
             }).OrderBy(x => x.Index).ToArray(),
-            Rankings = source.Ranking.Select(entry =>
+            Rankings = source.Ranking.Positions.Select(entry =>
             {
                 return new RankingDto
                 {
--- src/Turnierplan.Core/Tournament/Ranking.cs
+++ src/Turnierplan.Core/Tournament/Ranking.cs
@@ -1,42 +1,46 @@
-using System.Collections;
+using Turnierplan.Core.Exceptions;
 
 namespace Turnierplan.Core.Tournament;
 
-public sealed class Ranking : IEnumerable<RankingPosition>
+public sealed class Ranking
 {
     private readonly List<RankingPosition> _positions = [];
 
-    #region Access properties/methods and IEnumerable implementation
+    public bool IsEvaluated { get; private set; }
 
-    public IReadOnlyList<RankingPosition> Positions => _positions.AsReadOnly();
+    public IReadOnlyList<RankingPosition> Positions
+    {
+        get
+        {
+            ThrowIfNotEvaluated();
+            return _positions.AsReadOnly();
+        }
+    }
 
     public RankingPosition GetEntry(int position)
     {
+        ThrowIfNotEvaluated();
         return _positions.Single(x => x.Position == position);
     }
-
-    public IEnumerator<RankingPosition> GetEnumerator()
-    {
-        return _positions.GetEnumerator();
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
-
-    #endregion
-
-    #region Ranking evaluation logic
 
     internal void Evaluate(IReadOnlyCollection<Team> teams, IReadOnlyCollection<Match> matches)
     {
         _positions.Clear();
 
-        // TODO: Implement
+        // TODO: Implement new algorithm and insert into the temporary list
+        var positionsTemporary = new List<RankingPosition>();
 
-        _positions.Sort((a, b) => a.Position - b.Position);
+        _positions.AddRange(positionsTemporary
+            .OrderBy(x => x.Position)
+            .ThenBy(x => x.Team?.Name));
+        IsEvaluated = true;
     }
 
-    #endregion
+    private void ThrowIfNotEvaluated()
+    {
+        if (!IsEvaluated)
+        {
+            throw new TurnierplanException("The ranking cannot be accessed because it has not been evaluated yet.");
+        }
+    }
 }
