01475882f12901c73b1e05d63b1788e6ffe3b30e
Elias Hörner <elias.hoerner@hoerner-it.net>
2026-01-24 17:19:41 +0100

    Add endpoint

---
+++ src/Turnierplan.App/Endpoints/Images/SetImageNameEndpoint.cs
@@ -0,0 +1,64 @@
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Turnierplan.App.Extensions;
+using Turnierplan.App.Security;
+using Turnierplan.Core.PublicId;
+using Turnierplan.Dal.Repositories;
+
+namespace Turnierplan.App.Endpoints.Images;
+
+internal sealed class SetImageNameEndpoint : EndpointBase
+{
+    protected override HttpMethod Method => HttpMethod.Patch;
+
+    protected override string Route => "/api/images/{id}/name";
+
+    protected override Delegate Handler => Handle;
+
+    private static async Task<IResult> Handle(
+        [FromRoute] PublicId id,
+        [FromBody] SetImageNameEndpointRequest request,
+        IImageRepository repository,
+        IAccessValidator accessValidator,
+        CancellationToken cancellationToken)
+    {
+        if (!Validator.Instance.ValidateAndGetResult(request, out var result))
+        {
+            return result;
+        }
+
+        var image = await repository.GetByPublicIdAsync(id);
+
+        if (image is null)
+        {
+            return Results.NotFound();
+        }
+
+        if (!accessValidator.IsActionAllowed(image, Actions.GenericWrite))
+        {
+            return Results.Forbid();
+        }
+
+        image.Name = request.Name;
+
+        await repository.UnitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Results.NoContent();
+    }
+
+    public sealed record SetImageNameEndpointRequest
+    {
+        public required string Name { get; init; }
+    }
+
+    private sealed class Validator : AbstractValidator<SetImageNameEndpointRequest>
+    {
+        public static readonly Validator Instance = new();
+
+        private Validator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty();
+        }
+    }
+}
