7050cdc1376b1746d39133c2705a9f34eae3b0a4
Elias Hörner <elias.hoerner@hoerner-it.net>
2026-02-18 19:48:20 +0100

    Add comment to clarify folder authorization check (#346)

--- src/Turnierplan.App/Endpoints/Tournaments/CreateTournamentEndpoint.cs
+++ src/Turnierplan.App/Endpoints/Tournaments/CreateTournamentEndpoint.cs
@@ -56,6 +56,10 @@ internal sealed class CreateTournamentEndpoint : EndpointBase<TournamentDto>
             {
                 return Results.NotFound("The specified folder does not exist.");
             }
+
+            // No authorization check is necessary on the scope of the folder! This is because above, we ensure that
+            // the user has at least 'GenericWrite' permission on the organization. If he does, this permission is
+            // always passed onto the folder anyway.
         }
         else if (request.FolderName is not null)
         {
