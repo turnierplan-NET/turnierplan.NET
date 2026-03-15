b9159e5bc1860e25b7af493951252ac65a7dfb47
Elias Hörner <elias.hoerner@hoerner-it.net>
2026-03-15 11:40:28 +0100

    Fix failing e2e tests due to changes in previous commit (#367)

--- src/Turnierplan.App/Client/e2e/pages/landing-page.ts
+++ src/Turnierplan.App/Client/e2e/pages/landing-page.ts
@@ -5,7 +5,6 @@ export class LandingPage {
   constructor(private readonly page: Page) {}
 
   public async createOrganization(name: string): Promise<void> {
-    await this.page.getByTestId(turnierplan.pageFrame.navigationTab(turnierplan.landingPage.organizationsPageId)).click();
     await this.page.getByTestId(turnierplan.landingPage.newOrganizationButton).click();
     await this.page.getByTestId(turnierplan.createOrganizationPage.organizationNameField).fill(name);
     await this.page.getByTestId(turnierplan.createOrganizationPage.confirmButton).click();
