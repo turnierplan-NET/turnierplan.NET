c7a6cf9feffb71e9d1f9cf714fa01824987befe7
Elias Hörner <elias.hoerner@hoerner-it.net>
2026-05-03 08:54:34 +0200

    Implement migration

--- src/Turnierplan.ImageStorage/Local/LocalImageStorage.cs
+++ src/Turnierplan.ImageStorage/Local/LocalImageStorage.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
@@ -7,10 +8,12 @@ using Turnierplan.Core.Image;
 
 namespace Turnierplan.ImageStorage.Local;
 
-internal sealed class LocalImageStorage : ImageStorageBase, IMigratableImageStorage
+internal sealed partial class LocalImageStorage : ImageStorageBase, IMigratableImageStorage
 {
     private readonly ILogger<LocalImageStorage> _logger;
     private readonly string _storagePath;
+
+    private readonly SemaphoreSlim _migrationSemaphore = new(1, 1);
     private readonly bool _skipMigration;
 
     public LocalImageStorage(ILogger<LocalImageStorage> logger, IOptions<LocalImageStorageOptions> options)
@@ -33,7 +36,7 @@ internal sealed class LocalImageStorage : ImageStorageBase, IMigratableImageStor
 
     public override string GetFullImageUrl(Image image)
     {
-        return $"/images/{image.CreatedAt.Year}/{GetImageFileName(image)}";
+        return $"/images/{image.CreatedAt.Year}/{image.CreatedAt.Month:D2}/{GetImageFileName(image)}";
     }
 
     public override Task<bool> SaveImageAsync(Image image, MemoryStream imageData)
@@ -113,48 +116,112 @@ internal sealed class LocalImageStorage : ImageStorageBase, IMigratableImageStor
             return;
         }
 
+        /*
+         * This migration copies files from the previous folder structure to the new one:
+         *
+         * Previous folder structure:
+         *      <base path>/2025/5b22d459-58b8-4aac-8ad0-1b0ba5e453b3.webp
+         *      <base path>/2026/7e669676-443b-4d01-a12e-676c9c097739.webp
+         *
+         * New folder structure:
+         *      <base path>/2025/07/5b22d459-58b8-4aac-8ad0-1b0ba5e453b3.webp
+         *      <base path>/2026/04/7e669676-443b-4d01-a12e-676c9c097739.webp
+         *
+         * The new folder structure introduces a month-based subdirectory. This is done to be aligned with the
+         * other image storage implementations, which should make switching between implementations easier.
+         */
+
         try
         {
-            // TODO: Don't use version file and detect whether migration is necessary by looking at the files.
+            await _migrationSemaphore.WaitAsync(cancellationToken);
 
-            var version = "1";
-            var versionFile = Path.Join(_storagePath, ".version");
-
-            if (File.Exists(versionFile))
+            // First, we figure out if a migration needs to be run by checking if any files with the old naming scheme exist.
+            var hasOldFiles = Directory.GetDirectories(_storagePath).SelectMany(Directory.GetFiles).Any();
+            if (!hasOldFiles)
             {
-                version = await File.ReadAllTextAsync(versionFile, cancellationToken);
-            }
-
-            if (version.Equals("2"))
-            {
-                // Latest version, no migrations required
-                _logger.LogInformation("Storage of local image storage is up to date.");
+                _logger.LogInformation("The directory structure is up to date, no migration is needed.");
                 return;
             }
 
-            if (!version.Equals("1"))
-            {
-                _logger.LogCritical("Version file in local image storage path contains an unknown version '{Version}'.", version);
-                return;
-            }
+            _logger.LogInformation("A migration will be attempted because at least one file in the storage directory matches the old file pattern.");
 
+            // Load images from DB because we need the months of creation
             var images = await imageProvider.GetImagesAsync();
-            // TODO: Run migration by copying the files & writing new version number
-            // TODO: Display info message if any old files still exist
+
+            // Consistency check - there might be an error in the image provider implementation causing invalid data to be returned.
+            if (images.Count == 0)
+            {
+                _logger.LogWarning("The image provider returned no images even though the image storage folder contains old image files - migration is skipped.");
+                return;
+            }
+
+            // Iterate over all old files and handle them accordingly
+            foreach (var directory in Directory.GetDirectories(_storagePath))
+            {
+                foreach (var file in Directory.GetFiles(directory))
+                {
+                    var fileName = Path.GetFileName(file);
+                    var match = FileNameRegex().Match(fileName);
+
+                    if (!match.Success)
+                    {
+                        _logger.LogWarning("Encountered a file in the image storage directory which does not match the file name pattern: '{ImagePath}'", Path.GetFullPath(file));
+                        continue;
+                    }
+
+                    if (!Guid.TryParse(match.Groups["identifier"].Value, out var resourceIdentifier))
+                    {
+                        _logger.LogWarning("Encountered a file in the image storage directory which does not have a valid GUID in the file name: '{ImagePath}'", Path.GetFullPath(file));
+                        continue;
+                    }
+
+                    var image = images.FirstOrDefault(x => x.ResourceIdentifier == resourceIdentifier);
+
+                    if (image is null)
+                    {
+                        _logger.LogWarning("Encountered a file in the image storage directory for which there exists no corresponding entry from the image provider: '{ImagePath}'", Path.GetFullPath(file));
+                        continue;
+                    }
+
+                    var targetDirectory = Path.Join(_storagePath, image.CreatedAt.Year.ToString(CultureInfo.InvariantCulture), image.CreatedAt.Month.ToString("D2", CultureInfo.InvariantCulture));
+                    var targetPath = Path.Join(targetDirectory, fileName);
+
+                    Directory.CreateDirectory(targetDirectory);
+
+                    if (File.Exists(targetPath))
+                    {
+                        _logger.LogWarning("Encountered an already existing target file '{TargetPath}' while migrating '{SourcePath}' - the Move operation will be attempted again.", targetPath, file);
+                    }
+
+                    File.Move(file, targetPath, overwrite: true);
+                    _logger.LogInformation("Successfully moved image file '{TargetPath}' to '{SourcePath}'.", targetPath, file);
+                }
+            }
         }
         catch (Exception ex)
         {
             _logger.LogCritical(ex, "An unexpected exception occurred while trying to run local image storage migrations.");
         }
+        finally
+        {
+            _migrationSemaphore.Release();
+        }
     }
 
     private string GetImageFullPath(Image image)
     {
-        return Path.Join(_storagePath, image.CreatedAt.Year.ToString(CultureInfo.InvariantCulture), GetImageFileName(image));
+        return Path.Join(
+            _storagePath,
+            image.CreatedAt.Year.ToString(CultureInfo.InvariantCulture),
+            image.CreatedAt.Month.ToString("D2", CultureInfo.InvariantCulture),
+            GetImageFileName(image));
     }
 
     private static string GetImageFileName(Image image)
     {
         return $"{image.ResourceIdentifier}.{image.FileExtension}";
     }
+
+    [GeneratedRegex(@"(?<identifier>[0-9a-f-]{36})\.(?<extension>[a-z]+)$")]
+    private static partial Regex FileNameRegex();
 }
