786ff61f2a5a638a4cacae1a728e0a83f4886e55
Elias Hörner <elias.hoerner@hoerner-it.net>
2026-06-04 10:56:06 +0200

    Delete unused import

--- src/Turnierplan.App/Client/src/app/portal/pages/view-planning-realm/view-planning-realm.component.ts
+++ src/Turnierplan.App/Client/src/app/portal/pages/view-planning-realm/view-planning-realm.component.ts
@@ -37,7 +37,6 @@ import { updatePlanningRealm } from '../../../api/fn/planning-realms/update-plan
 import { deletePlanningRealm } from '../../../api/fn/planning-realms/delete-planning-realm';
 import { LabelsManagerComponent } from '../../components/labels-manager/labels-manager.component';
 import { ExportApplicationsDialogComponent } from '../../components/export-applications-dialog/export-applications-dialog.component';
-import { ApiKeyExtendComponent } from '../../components/api-key-extend/api-key-extend.component';
 
 export type UpdatePlanningRealmFunc = (modifyFunc: (planningRealm: PlanningRealmDto) => boolean) => void;
 
