19d25223a482922658ebdd87bd2c96d8c92398aa
Elias Hörner <elias.hoerner@hoerner-it.net>
2026-07-10 22:15:04 +0200

    Add basic UI framework

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -3,6 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+export const appVersion = '2026.3.0';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { appVersion } from './app.config';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'tp-root',
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterLink],
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('turnierplan-net');
+  protected readonly version = signal(appVersion);
 }
