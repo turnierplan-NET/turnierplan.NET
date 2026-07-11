51cb1e4b5f7a8b28871aabdaa036d12f8caab3e2
Elias Hörner <elias.hoerner@hoerner-it.net>
2026-07-11 09:15:15 +0200

    Fix app component test

--- web/src/app/app.spec.ts
+++ web/src/app/app.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { provideRouter } from '@angular/router';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [provideRouter([])],
     }).compileComponents();
   });
 
@@ -14,10 +16,12 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render application name in sidebar', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, turnierplan-net');
+    expect(compiled.querySelector('.tp-sidebar-branding')?.textContent).toContain(
+      'turnierplan.NET',
+    );
   });
 });
