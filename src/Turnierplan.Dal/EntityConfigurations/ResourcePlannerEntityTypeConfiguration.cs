9d963dd5512b2be12a5b2c3f53f58dda231aa2c8
Elias Hörner <elias.hoerner@hoerner-it.net>
2026-07-15 21:57:43 +0200

    Add entity type config & integrate into organization

---
+++ src/Turnierplan.Dal/EntityConfigurations/ResourcePlannerEntityTypeConfiguration.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Turnierplan.Core.ResourcePlanner;
+using Turnierplan.Dal.Converters;
+
+namespace Turnierplan.Dal.EntityConfigurations;
+
+public sealed class ResourcePlannerEntityTypeConfiguration : IEntityTypeConfiguration<ResourcePlanner>
+{
+    public void Configure(EntityTypeBuilder<ResourcePlanner> builder)
+    {
+        builder.ToTable("ResourcePlanners", TurnierplanContext.Schema);
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .IsRequired();
+
+        builder.Property(x => x.PublicId)
+            .HasConversion<PublicIdConverter>();
+
+        builder.HasIndex(x => x.PublicId)
+            .IsUnique();
+
+        builder.HasMany(x => x.RoleAssignments)
+            .WithOne(x => x.Scope)
+            .HasForeignKey("ResourcePlannerId")
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired();
+
+        builder.Property(x => x.CreatedAt)
+            .IsRequired();
+
+        builder.Property(x => x.Name)
+            .IsRequired();
+
+        builder.Metadata.FindNavigation(nameof(ResourcePlanner.RoleAssignments))!.SetPropertyAccessMode(PropertyAccessMode.Field);
+    }
+}
