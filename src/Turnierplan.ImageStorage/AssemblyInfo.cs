9662efb663a918b225274ecfd1e8fb1ab75d2422
Elias Hörner <elias.hoerner@hoerner-it.net>
2026-07-28 16:25:23 +0200

    Adjust wording

--- src/Turnierplan.ImageStorage/AssemblyInfo.cs
+++ src/Turnierplan.ImageStorage/AssemblyInfo.cs
@@ -2,5 +2,6 @@
 
 [assembly: InternalsVisibleTo("Turnierplan.ImageStorage.Test.Migration")]
 
-// TODO: When image storage projects are refactored, each implementation should have a testable public API and the following line should not be necessary
+// TODO: Once the image storage projects are refactored, each implementation should have
+//       a testable public API and the following line should no longer be necessary.
 [assembly: InternalsVisibleTo("Turnierplan.ImageStorage.Test.Integration")]
