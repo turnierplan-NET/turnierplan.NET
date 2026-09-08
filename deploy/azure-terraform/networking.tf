07fdaa4532bd52ab77153fc90f295e170955ebd7
Elias Hörner <elias.hoerner@hoerner-it.net>
2026-09-08 11:48:32 +0200

    Update

--- deploy/azure-terraform/networking.tf
+++ deploy/azure-terraform/networking.tf
@@ -25,6 +25,10 @@ resource "azurerm_subnet" "database" {
   resource_group_name  = azurerm_resource_group.default.name
   virtual_network_name = azurerm_virtual_network.default.name
   address_prefixes     = ["10.0.2.0/24"]
+  
+  service_endpoint {
+    service = "Microsoft.Storage"
+  }
 
   delegation {
     name = "psql"
